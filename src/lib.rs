2c4f429f666062bc99cbe118a05a6413a294928e
Zach Silver <zsilver96@gmail.com>
2017-12-02 13:56:36 -0500

    completed interpreter

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+use std::io::{self, Read};
 
 pub struct Tape {
     tape: [u8; 10000],
@@ -36,31 +37,63 @@ impl Tape {
     }
 
     fn output(&self) {
-        println!("{}", self.tape[self.pointer] as char);
+        print!("{}", self.tape[self.pointer] as char);
     }
 
     fn input(&mut self) {
-        
+        let input = io::stdin()
+            .bytes() 
+            .next()
+            .and_then(|result| result.ok());
+        match input {
+            None => panic!("input error"),
+            Some(c) => self.tape[self.pointer] = c
+        }
     }
 
     fn jump_f(&mut self) {
-        
+        if self.tape[self.pointer] == 0 {
+            self.cur_op = match self.map.get(&self.cur_op) {
+                Some(i) => *i,
+                None => panic!("forward jump not found")
+            };
+        }
     }
 
     fn jump_b(&mut self) {
-        
+        if self.tape[self.pointer] != 0 {
+            self.cur_op = match self.map.get(&self.cur_op) {
+                Some(i) => *i,
+                None => panic!("backward jump not found")
+            };
+        }
     }
 
     fn finished(&self) -> bool {
-        self.cur_op >= ops.len()
+        self.cur_op >= self.ops.len()
     }
 
-    fn execute_instruction(&self) {
-        
+    fn execute_instruction(&mut self) {
+        let op = *self.ops.get(self.cur_op).expect("instruction error");
+        self.perform_op(op);
+        self.cur_op += 1;
+    }
+
+    fn perform_op(&mut self, op: Op) {
+        match op {
+            Op::Inc => self.inc(),
+            Op::Dec => self.dec(),
+            Op::IncByte => self.inc_byte(),
+            Op::DecByte => self.dec_byte(),
+            Op::Output => self.output(),
+            Op::Input => self.input(),
+            Op::JumpF => self.jump_f(),
+            Op::JumpB => self.jump_b(),
+        }
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy)]
 pub enum Op {
     Inc,
     Dec,
@@ -72,28 +105,11 @@ pub enum Op {
     JumpB
 }
 
-impl Op {
-    pub fn perform(&self, tape: &mut Tape) {
-        match *self {
-            Op::Inc => tape.inc(),
-            Op::Dec => tape.dec(),
-            Op::IncByte => tape.inc_byte(),
-            Op::DecByte => tape.dec_byte(),
-            Op::Output => tape.output(),
-            Op::Input => tape.input(),
-            Op::JumpF => tape.jump_f(),
-            Op::JumpB => tape.jump_b(),
-        }
-    }
-}
-
-
 pub fn run(code: &str) -> Result<(), String> {
     let mut jump_map = HashMap::new();
     let ops: Vec<Op> = parse(code, &mut jump_map)?;
-    println!("{:?}", ops);
     let mut tape = Tape::new(ops, jump_map);
-    while (!tape.finished()) {
+    while !tape.finished() {
         tape.execute_instruction();
     }
     Ok(())
@@ -124,7 +140,6 @@ pub fn parse(code: &str,
         ops.push(o);
         counter += 1;
     }
-    println!("Jump Map: {:?}\n", map);
     Ok(ops)
 }
 
@@ -140,8 +155,8 @@ fn parse_jump_b(counter: usize,
                 map: &mut HashMap<usize, usize>) -> Op {
     match v.pop() {
         Some(c) => {
-            map.insert(counter, c + 1);
-            map.insert(c, counter + 1);
+            map.insert(counter, c);
+            map.insert(c, counter);
         }
         None => panic!("unbalanced parens at {}", counter)
     }
